c80d04acea0bbc474e7bc5eb0a0d61502bd396d5
shaunam1 <S00273772@atu.ie>
2026-09-21 15:32:56 +0100

    Tasks 1 - 4

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+import express, {Application, Request, Response} from "express"; 
+const PORT = process.env.PORT || 8080; 
+const app: Application = express(); 
+
+app.get("/ping", async (_req : Request, res: Response) => { 
+    res.json({ 
+    message: "hello from Shauna. Web Programming 2 Exercise 1 "
+    }); 
+
+}); 
+
+app.get('/bananas', async (_req : Request, res: Response) => { 
+    res.json({ 
+    message: "this is bananas", 
+}); 
+
+}); 
+
+app.get('/pineapples', async (_req : Request, res: Response) => { 
+    res.json({ 
+    message: "this is pineapples", 
+}); 
+
+}); 
+
+app.get('/squirrels', async (_req : Request, res: Response) => { 
+    res.json({ 
+    message: "this is squirrels", 
+}); 
+
+}); 
+
+app.get('/hellokitty', async (_req : Request, res: Response) => { 
+    res.json({ 
+    message: "this is Hello Kitty", 
+}); 
+
+}); 
+
+app.get('/purple', async (_req : Request, res: Response) => { 
+    res.json({ 
+    message: "this is purple", 
+}); 
+
+}); 
+
+app.use((req, _res, next) => { 
+    console.log(`${req.method} ${req.originalUrl}`); 
+    next(); 
+
+}); 
+ 
+app.listen(PORT, () => { 
+    console.log("Server is running on port", PORT); 
+    }); 
