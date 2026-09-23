ad0e1d461621c13d54cb3d91b4baefa70b9b7551
s00273772 <s00273772@atu.ie>
2026-09-23 09:42:11 +0100

    Task 1 completed

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
 import express, {Application, Request, Response} from "express"; 
 import carRoutes from './routes/cars';
+import { env } from "./config/env";
 
-const PORT = process.env.PORT || 3000; 
+const PORT = env.port
 const app: Application = express(); 
 
 app.get("/ping", async (_req : Request, res: Response) => { 
