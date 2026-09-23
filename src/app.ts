e0a89e41278eb359e8fca74b2c1d9d86c4f536df
s00273772 <s00273772@atu.ie>
2026-09-23 10:12:14 +0100

    database connected

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {Application, Request, Response} from "express"; 
 import carRoutes from './routes/cars';
 import { env } from "./config/env";
+import {connectDB} from "./config/database"
 
 const PORT = env.port
 const app: Application = express(); 
@@ -59,6 +60,16 @@ app.use('/api/v1/cars', carRoutes);
 
 
  
-app.listen(PORT, () => { 
-    console.log("Server is running on port", PORT); 
-    }); 
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+startServer();
+
+
+   
+    
--- src/config/env.ts
+++ src/config/env.ts
@@ -6,5 +6,7 @@ export const env = {
 
 port: Number(process.env.PORT ?? 3001),
 nodeEnv: process.env.NODE_ENV ?? "development",
+mongoURI: process.env.MONGODB_URI ?? ""
 
 };
+
